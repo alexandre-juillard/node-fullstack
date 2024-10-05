ae0a1fbb79eca2a2de274a4caf9d3f3ad95fa6f9
alexandre-juillard <ajuillard69@gmail.com>
2024-10-05 18:31:19 +0200

    ajout CRUD complet + model Thing

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const Thing = require('./models/thing');
+
+mongoose.connect('mongodb+srv://alexandre:Txf7zow8J307dwmQ@cluster0.k98hw.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 //middleware qui capture le corps des requêtes et les transforme en objet JSON
 app.use(express.json());
@@ -12,42 +18,56 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
+//Crée un objet
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
     });
-  });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+});
 
+//Récupère tous les objets
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+    Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
+//Récupère un objet spécifique
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+//Modifie un objet
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+//Supprime un objet
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 
 module.exports = app;
 
 
 //on importe express
+//on importe mongoose pour se connecter à la base de données
 //on stocke express dans la constante app
+//on se connecte à la base de données avec mongoose.connect
 //on exporte app pour l'utiliser dans d'autres fichiers
 //on utilise app.use pour créer une route qui renvoie un objet JSON
+//on utilise app.get pour créer une route qui renvoie un tableau d'objets
 
 //avec next() on passe à la fonction suivante
 //cela permet de chaîner les middlewares
---
+++ backend/models/Thing.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
+
+//on importe mongoose
+//on crée un schéma pour les objets avec mongoose.Schema
+//on exporte le modèle Thing avec mongoose.model
