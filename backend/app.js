49ff19f12664d63ff8a055a9a439ea2b0e73b1b1
alexandre-juillard <ajuillard69@gmail.com>
2024-10-05 12:23:55 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const app = express();
+
+//middleware qui capture le corps des requêtes et les transforme en objet JSON
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+
+module.exports = app;
+
+
+//on importe express
+//on stocke express dans la constante app
+//on exporte app pour l'utiliser dans d'autres fichiers
+//on utilise app.use pour créer une route qui renvoie un objet JSON
+
+//avec next() on passe à la fonction suivante
+//cela permet de chaîner les middlewares
---
+++ backend/server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const app = require('./app');
+
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+server.listen(port);
+
+//on initialise le projet node avec cmd npm init
+//le server node est démarré avec createServer du package http
+//nodemon est installé pour redémarrer le server à chaque modification
+//on importe app depuis app.js
+//on définit le port avec app.set
+
+//le server est démarré avec node server ou nodemon server
+//le server est accessible sur localhost:3000
+//on peut changer le port avec process.env.PORT
+//on gère les erreurs avec errorHandler
+//normalizePort renvoie un port valide
