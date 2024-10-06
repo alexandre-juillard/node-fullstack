3a8ac30ee902d966be0af98e98c4b8918657c294
alexandre-juillard <ajuillard69@gmail.com>
2024-10-06 18:03:02 +0200

    ajout jwt + token sur les routes

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,22 +1,23 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 const Thing = require('../models/thing');
 const stuffCtrl = require('../controllers/stuff');
 
 //Crée un objet
-router.post('/', stuffCtrl.createThing);
+router.post('/', auth, stuffCtrl.createThing);
 
 //Récupère tous les objets
-router.get('/', stuffCtrl.getAllThings);
+router.get('/', auth, stuffCtrl.getAllThings);
 
 //Récupère un objet spécifique
-router.get('/:id', stuffCtrl.getOneThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
 
 //Modifie un objet
-router.put('/:id', stuffCtrl.updateOneThing);
+router.put('/:id', auth, stuffCtrl.updateOneThing);
 
 //Supprime un objet
-router.delete('/:id', stuffCtrl.deleteThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
